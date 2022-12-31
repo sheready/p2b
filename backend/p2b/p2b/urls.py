7c6a5c8e699a7f264d79ce36f0d20fc523eefcf8
sheready <csstlw@gmail.com>
2022-12-31 22:22:22 +0300

    edit url

--- backend/p2b/p2b/urls.py
+++ backend/p2b/p2b/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lms/', include('lms.urls')),
+    path('api/lms/', include('lms.urls')),
 
 ]
