32334f4e84bdd013f4f9fa22ce95a48d5a468584
sheready <csstlw@gmail.com>
2022-12-31 21:58:03 +0300

    add footer

---
+++ frontend/p2b/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="container-footer">
+    &copy;2023 | <a href="https://passporttobusiness.com">Passport To Business</a>
+  </footer>
+  )
+}
+
+export default Footer
