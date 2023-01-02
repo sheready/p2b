6a31584d76bb35474a0978938d654c6079c062dc
sheready <csstlw@gmail.com>
2023-01-02 23:47:52 +0300

    edit the delete url to include the retrieve,update and delete view

--- backend/p2b/lms/urls.py
+++ backend/p2b/lms/urls.py
@@ -5,8 +5,6 @@ from lms.views import *
 urlpatterns = [
     path('courses/', CourseList.as_view(), name='courses'),
     path('courses/create', CourseCreate.as_view(), name='create-course'),
-    path('courses/<int:id>/delete', CourseDelete.as_view(), name='delete_course'),
-    path('courses/<str:pk>/update', views.updateCourse, name='update-course'),
-    path('courses/<str:pk>/delete', views.deleteCourse, name='delete-course'),
-    path('courses/<str:pk>/', views.getCourse, name='course'),
+    path('courses/<int:id>/', CourseRetrieveUpdateDelete.as_view(), name='delete_course'),
+
 ]
