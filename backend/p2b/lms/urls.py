bd1481912944e9c2c81b29988d0bbdf73585b832
sheready <csstlw@gmail.com>
2023-01-02 23:12:20 +0300

    add the course delete route

--- backend/p2b/lms/urls.py
+++ backend/p2b/lms/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from lms.views import *
 from . import views
+from lms.views import *
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
     path('courses/', CourseList.as_view(), name='courses'),
     path('courses/create', CourseCreate.as_view(), name='create-course'),
+    path('courses/<int:id>/delete', CourseDelete.as_view(), name='delete_course'),
     path('courses/<str:pk>/update', views.updateCourse, name='update-course'),
     path('courses/<str:pk>/delete', views.deleteCourse, name='delete-course'),
     path('courses/<str:pk>/', views.getCourse, name='course'),
