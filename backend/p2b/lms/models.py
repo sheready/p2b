4cd8bae241bc2541068e5dad80d20f4fe650411b
sheready <csstlw@gmail.com>
2023-01-03 22:26:48 +0300

    add image field and modify cost to float field from decimal field

--- backend/p2b/lms/models.py
+++ backend/p2b/lms/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class Course(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.CharField(max_length=100, blank=True, default='')
-    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    cost = models.FloatField(max_digits=6, decimal_places=2)
+    photo = models.ImageField(blank=True, null=True, default=None, upload_to='products')
 
