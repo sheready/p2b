f5457297896b599d3ed291b96785f9cd484d6197
sheready <csstlw@gmail.com>
2023-04-07 09:50:54 +0300

    add video model field

--- backend/p2b/lms/models.py
+++ backend/p2b/lms/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.core.validators import FileExtensionValidator
 # Create your models here.
 
 
 class Course(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.CharField(max_length=100, blank=True, default='')
-    cost = models.FloatField(max_digits=6, decimal_places=2)
-    photo = models.ImageField(blank=True, null=True, default=None, upload_to='products')
+    cost = models.FloatField()
+    photo = models.ImageField(blank=True, null=True, default=None, upload_to='uploads/images')
+    video = models.FileField(upload_to='uploads/videos',null=True,validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
 
